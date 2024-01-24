90d3a28249f52479613f573c126d75561ea49822
AbdSaleh97 <salehabd.897@gmail.com>
2024-01-24 21:19:06 +0300

    Employee card

--- jsFiles/app.js
+++ jsFiles/app.js
@@ -1,15 +1,18 @@
 'use strict';
 
-let empArr = [];
+let formElement = document.getElementById("form");
+let department = document.getElementById("dep");
+let level = document.getElementById("lvl");
+let img = document.getElementById("imgUrl");
+let btn = document.getElementById("subBtn");
 
-let Employee = function (empId, fullName, Dep, level, img) {
-    this.empId = empId;
+let Employee = function (fullName, Dep, level, img) {
+    this.empId = null;
     this.fullName = fullName;
     this.Dep = Dep;
     this.level = level;
     this.img = img;
     this.salary = 1;
-    empArr.push(this);
 }
 
 Employee.prototype.calcSalary = function () {
@@ -27,7 +30,6 @@ Employee.prototype.calcSalary = function () {
         min = 1000;
         sal = Math.floor(Math.random() * (max - min + 1) + min);
         this.salary = sal - (sal * .075);
-
     }
     else {
         max = 1000;
@@ -37,44 +39,82 @@ Employee.prototype.calcSalary = function () {
 
     }
 }
-let element = document.getElementById('main');
-    
-    element.innerHTML = `
-        <table border="1">
-            <tr>
-                <th>Employee ID</th>
-                <th>Full Name</th>
-                <th>Department</th>
-                <th>Level</th>
-                <th>Salary</th>
-            </tr>
-        </table>
-    `;
 
 Employee.prototype.render = function () {
+    let main = document.getElementById("main");
 
-    let table = document.querySelector('table');
-    table.innerHTML += `
-<tr>
-    <td>${this.empId}</td>
-    <td>${this.fullName}</td>
-    <td>${this.Dep}</td>
-    <td>${this.level}</td>
-    <td>$${this.salary}</td>
-</tr>
-`;
+    let card = document.createElement("div");
+
+    let imgDiv = document.createElement("img");
+    imgDiv.src = this.img;
+    imgDiv.alt = "img";
+    imgDiv.style.width = '100%';
+    imgDiv.style.borderRadius = '50%';
+    imgDiv.style.marginBottom = '15px';
+    
+    card.appendChild(imgDiv);
+    console.log(imgDiv);
+
+    let name = document.createElement("p");
+    name.innerText = "Name: " + this.fullName;
+    card.appendChild(name);
+    console.log(name);
+
+
+    let id = document.createElement("p");
+    id.innerText = "ID: " + this.empId;
+    card.appendChild(id);
+    console.log(id);
+
+    let department = document.createElement("p");
+    department.innerText = "Department: " + this.Dep;
+    card.appendChild(department);
+    console.log(department);
+
+    let level = document.createElement("p");
+    level.innerText = "Level: " + this.level;
+    card.appendChild(level);
+    console.log(imgDiv);
+
+    let salary = document.createElement("p");
+    salary.innerText = "Salary: " + this.salary;
+    card.appendChild(salary);
+    console.log(salary);
+
+    card.style.maxWidth = '300px';
+    card.style.margin = '20px';
+    card.style.padding = '20px';
+    card.style.backgroundColor = '#fff';
+    card.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+    card.style.textAlign = 'center';
+
+    main.appendChild(card);
 };
 
-let emp1 = new Employee(12, "abdelrahman", "Administration", "senior", "img1.jpg");
-let emp2 = new Employee(2, "Jane Smith", "Marketing", "mid-senior", "img2.jpg");
-let emp3 = new Employee(3, "Bob Johnson", "Finance", "Junior", "img3.jpg");
-let emp4 = new Employee(4, "Alice Johnson", "HR", "senior", "img4.jpg");
-let emp5 = new Employee(5, "Eva Williams", "Development", "mid-senior", "img5.jpg");
+formElement.addEventListener("submit", submitHandler);
+
+function submitHandler(event) {
+
+    event.preventDefault();
+    console.log(event);
+    let fullName = event.target.fullName.value;
+    let dep = event.target.dep.value;
+    let level = event.target.lvl.value;
+    let img = event.target.imgUrl.value;
+
+    
+    let obj = new Employee(fullName, dep, level, img);
+    randEmpId(obj);
+    obj.calcSalary();
+    obj.render();
+
+}
 
 
-for (let index = 0; index < empArr.length; index++) {
-    empArr[index].calcSalary();
-    empArr[index].render();
+
+function randEmpId(emp) {
+    // source code =>  https://developer.mozilla.org/en-US/docs/Web/API/Crypto/randomUUID
+    emp.empId = crypto.randomUUID().slice(32);
 }
 
 
